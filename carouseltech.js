1b7a9cd87b50a8d6f71616c9f65b967ed804e85b
Ayelén  Campot <ayelen.campot@devecoop.com>
2023-03-15 12:25:56 -0300

    cambios varios

---
+++ carouseltech.js
@@ -0,0 +1,12 @@
+new Glide('.glide', {
+    type: 'carousel',
+    perView: 5,
+    breakpoints: {
+        800: {
+            perView: 4
+        },
+    600: {
+            perView: 2
+        }
+    }
+}).mount();
